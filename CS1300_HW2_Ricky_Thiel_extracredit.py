35bf9361bcd6a9a0fdc6693b66e8a81d15bc5e10
Richard <rrthiel01@indianatech.net>
2026-03-05 13:33:22 -0500

    check in hw2

---
+++ CS1300_HW2_Ricky_Thiel_extracredit.py
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+text = input("Enter a word: ")
+
+cleaned = text.replace(" ", "").lower()
+
+if cleaned == cleaned[::-1]:
+    print("Palindrome")
+else:
+    print("Not a palindrome")
+=======
+text = input("Enter a word: ")
+
+cleaned = text.replace(" ", "").lower()
+
+if cleaned == cleaned[::-1]:
+    print("Palindrome")
+else:
+    print("Not a palindrome")
+>>>>>>> 8743aea8878413d0256b83391710118ac80220ee
